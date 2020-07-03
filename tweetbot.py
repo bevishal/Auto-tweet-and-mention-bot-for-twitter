32062ccf11a18bd34390a3fd4dac982b16792007
Vishal Pandey <42720880+bevishal@users.noreply.github.com>
2020-07-03 17:51:04 +0530

    Update tweetbot.py
    Added some comments

--- tweetbot.py
+++ tweetbot.py
@@ -1,12 +1,13 @@
 #tweet bot using python and tweepy.
-#python and tweepy required to be installed.
+#python and tweepy required.
 import tweepy
 from time import sleep
 import random
 
-#Go to developer.twitter.com
-#sign up for devoloper account then create an app and your unque Keys and secret.
-CONSUMER_KEY = 'xxxxxx'
+#Setup your twitter dev account on https://developer.twitter.com/
+#to know more read this blog https://docs.inboundnow.com/guide/create-twitter-application/
+
+CONSUMER_KEY = 'Paste your keys'
 CONSUMER_SECRET = 'yyyyyy'
 ACCESS_KEY = 'zzzzz'
 ACCESS_SECRET = 'zzz'
@@ -17,8 +18,8 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-#twitter compares the tweets multiple tweet can not have same value, so we are going to generate random value an add that to our every tweet.
-#generating random number(int) to add in last of the tweet so it don't look same
+#twitter compares the tweets. multiple tweet can not have same value, so we are going to generate random value an add that to our every tweet.
+#generating random number(int).
 randomlist = random.sample(range(0, 99), 99)
 int = random.choice(randomlist)
 print(int)
@@ -31,6 +32,6 @@ for status in api.user_timeline():
     #items(no.) no. of time you want to tweet
     api.update_status('YOUR TEXT TO BE TWEETED @User_name #Hashtags ' + ' ' + string).items(100)
     print('Comment successful')
-    #Read the twitter automaation guidlines don't get blocked.
+    #Read the twitter automaation guidlines, don't get blocked.
     #sleep(60) In second
     sleep(60)
